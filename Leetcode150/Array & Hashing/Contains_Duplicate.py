3468ecfbcbca62690ad615e792e5077f65784859
Subash1237 <rimalsubash77@gmail.com>
2023-09-16 15:21:33 +0545

    commsdf

--- Leetcode150/Array & Hashing/Contains_Duplicate.py
+++ Leetcode150/Array & Hashing/Contains_Duplicate.py
@@ -1,8 +1,13 @@
 class Solution:
-    def containsDuplicate(self,nums:List[int])->bool:
+    def containsDuplicate(self,nums:list[int])->bool:
         hashset=set()
         for n in nums: 
             if n in hashset :
                 return True
             hashset.add(n)
         return False
+
+
+# Path: Leetcode150/Array & Hashing/Contains_Duplicate.py
+Solution=Solution()
+print(Solution.containsDuplicate([1,2,3,1]))
