3549ace57cfdeffde44fa29ad0cc88e7ddb4bfbf
Subash1237 <rimalsubash77@gmail.com>
2023-09-16 16:52:13 +0545

    done part 2 of it using sorting and comparing adj.

--- Leetcode150/Array & Hashing/Twosum.py
+++ Leetcode150/Array & Hashing/Twosum.py
@@ -1,17 +1,31 @@
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        print("i am total num",len(nums))
+
+        '''
+# using nested for loop 
+
         for i in range(len(nums)):
             for j in range((i+1),len(nums)): 
                 if (nums[i]+nums[j]==target):
                     return True
         return False        
-#Time complexity of this problem is O(n^2)  
-# space complexity of this problem is O(1) 
+Time complexity of this problem is O(n^2)  
+space complexity of this problem is O(1) 
 
+'''
 
+# Path: Leetcode150/Array & Hashing/Twosum.py
+# using sorting and comparing the addition of adjacents values 
+
+        nums=sorted(nums)
+        for i in range(len(nums)-1):
+            if(nums[i]+nums[i+1])==target:
+                return True
+
+        return False
+# it's time complexity is O(nlogn) and space complexity is O(1)
 
 solver=Solution()
-print(solver.twoSum([1,2,3,4,5],69))
+print(solver.twoSum([1,2,3,4,5,6,7,8,9],10))
 
 
