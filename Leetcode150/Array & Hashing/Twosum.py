f5b3c9bd0dbc2733fe836a96c8e76c5419030972
Subash1237 <rimalsubash77@gmail.com>
2023-09-16 16:40:07 +0545

    soluton 1 completed for Twosum.py worst solution.

--- Leetcode150/Array & Hashing/Twosum.py
+++ Leetcode150/Array & Hashing/Twosum.py
@@ -1,16 +1,17 @@
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         print("i am total num",len(nums))
-        for i in range(0,len(nums)-1): # is same as for i in range(len(nums)): because it's start from 0
-      # so it will iterate from 0 to len(nums)-1 i.e 0 to 4
-            for j in range((i+1),len(nums)): # and this will iterate from 1 to len(nums) i.e 1 to 5
-                
-                print("comapring",i,j)
+        for i in range(len(nums)):
+            for j in range((i+1),len(nums)): 
                 if (nums[i]+nums[j]==target):
                     return True
         return False        
+#Time complexity of this problem is O(n^2)  
+# space complexity of this problem is O(1) 
 
 
 
 solver=Solution()
 print(solver.twoSum([1,2,3,4,5],69))
+
+
