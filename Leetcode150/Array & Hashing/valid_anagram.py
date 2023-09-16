82f1005e33f32f0959508f3054b0e3b132f7b21f
Subash1237 <rimalsubash77@gmail.com>
2023-09-16 16:14:46 +0545

    valid_anagram.py completed

---
+++ Leetcode150/Array & Hashing/valid_anagram.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self,s:str,t:str)->bool:
+
+      #   return sorted(s)==sorted(t)  # Solution 1  is perfect solution for this 
+      #   return counter(s)==counter(t)  # Solution 2  is same as Hashmap but more efficent because it's builtin hashmap like 
+
+       # Using Hashmap regural using dict,  
+        if len(s)!=len(t):  
+            return False 
+        dictS,dictT={},{} 
+        for i in range(len(s)):
+            dictS[s[i]]=1+dictS.get(s[i],0)  # gives value of s[i] , if not exits then provide 0 
+            dictT[s[i]]=1+dictT.get(s[i],0)
+        return dictT==dictS
+
+
+Solver=Solution()
+print(Solver.isAnagram("anagram","nagrama"))
