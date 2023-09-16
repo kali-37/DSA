7c7396597a9941a33ec8f9744fd6baf3f887f1e1
Subash1237 <rimalsubash77@gmail.com>
2023-09-16 17:16:51 +0545

    hashmap doing

--- Leetcode150/Array & Hashing/Twosum.py
+++ Leetcode150/Array & Hashing/Twosum.py
@@ -7,23 +7,20 @@ class Solution:
         for i in range(len(nums)):
             for j in range((i+1),len(nums)): 
                 if (nums[i]+nums[j]==target):
-                    return True
+                    return [i,j]
         return False        
 Time complexity of this problem is O(n^2)  
 space complexity of this problem is O(1) 
 
 '''
+# using hashmap 
 
-# Path: Leetcode150/Array & Hashing/Twosum.py
-# using sorting and comparing the addition of adjacents values 
-
-        nums=sorted(nums)
-        for i in range(len(nums)-1):
-            if(nums[i]+nums[i+1])==target:
-                return True
-
-        return False
-# it's time complexity is O(nlogn) and space complexity is O(1)
+        hashmap={}
+        for i,j in enumerate(nums):
+            if not hashmap[target-j]:
+                target[j]=i
+            else:
+                return [hashmap[target-j].value(),i] 
 
 solver=Solution()
 print(solver.twoSum([1,2,3,4,5,6,7,8,9],10))
