9b3ad59d4391e2df75d56f13c6bbb6263faf1ebb
Subash1237 <rimalsubash77@gmail.com>
2023-09-17 17:53:56 +0545

    solved topKfrequent_elemes.py in 0(n) ..

--- Leetcode150/Array & Hashing/topKfrequent_elems.py
+++ Leetcode150/Array & Hashing/topKfrequent_elems.py
@@ -1,21 +1,43 @@
 #  https://leetcode.com/problems/top-k-frequent-elements/
 
+# class Solution:
+#     def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+#         hashtable={} 
+#         for i in nums:
+#             if i  in hashtable:
+#                 hashtable[i]+=1
+#             else:
+#                 hashtable[i]=1
+#         rearrange=sorted(list(hashtable.values()),reverse=True)[:k]
+#         returner=[]
+#         print("rearrange",rearrange[:3])
+#         for key,value in hashtable.items():
+#                 if value in rearrange:
+#                     print("value",value, "key",key)
+#                     returner.append(key)
+#         return returner
+
+# As , this take nlog(n) times , so let's do it within 0(n) times using bucket sorting. 
+
+
+
 class Solution:
     def topKFrequent(self, nums: list[int], k: int) -> list[int]:
-        hashtable={} 
+        dicter={}
+        lister=[[] for i in range(len(nums)-1)] 
         for i in nums:
-            if i  in hashtable:
-                hashtable[i]+=1
-            else:
-                hashtable[i]=1
-        rearrange=sorted(list(hashtable.values()),reverse=True)[:k]
-        returner=[]
-        print("rearrange",rearrange[:3])
-        for key,value in hashtable.items():
-                if value in rearrange:
-                    print("value",value, "key",key)
-                    returner.append(key)
-        return returner
+            dicter[i]=1+dicter.get(i,0)
+        
+        for key,value in dicter.items():
+            lister[value].append(key)
+        answer=[]
+        for i in range(len(lister)-1,0,-1):
+            for j in lister[i]:
+                print(j)
+                answer.append(j)
+                if (k==len(answer)):
+                    return answer 
+
 
 solver=Solution()
 print(solver.topKFrequent([1,2,3,4,1,1,2,1,2,11,23,23,234,32,1,12,123,123,12,213,123,123,123,123],3) )
