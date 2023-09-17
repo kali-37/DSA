f4792608800239f2e52afd19678a62e2c5e8adec
Subash1237 <rimalsubash77@gmail.com>
2023-09-17 09:23:34 +0545

    half done

---
+++ Leetcode150/Array & Hashing/tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+        # time.sleep(323)
+        # for i,s in enumerate(sorted_array):
+        #     si=0
+        #     for c in dicter:
+        #         print(c)
+        #         if sorted_array[c[0]]==s:
+        #             si=1
+        #             # dicter[c].append(i)
+        #     if (si==0):
+        #         print(123)
+        #         dicter.append([i])
