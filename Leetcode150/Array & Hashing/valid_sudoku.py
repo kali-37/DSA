42e7624fa66cd41955679d55843316452fbe097c
Subash1237 <rimalsubash77@gmail.com>
2023-09-18 16:58:44 +0545

    few changes , list do have append not add ..

--- Leetcode150/Array & Hashing/valid_sudoku.py
+++ Leetcode150/Array & Hashing/valid_sudoku.py
@@ -10,11 +10,10 @@ class Solution:
             for c in range(9):
                 if board[r][c]!=".":
                     if( board[r][c] in rowcheck[r] or board[r][c] in columncheck[c] or board[r][c] in squarecheck[(r//3,c//3)] ):
-                        print(r,c)
                         return False
-                    rowcheck[r].add(board[r][c])
-                    columncheck[c].add(board[r][c])
-                    squarecheck[(r//3,c//3)].add(board[r][c] )
+                    rowcheck[r].append(board[r][c])
+                    columncheck[c].append(board[r][c])
+                    squarecheck[(r//3,c//3)].append(board[r][c] )
 
         return True
 
