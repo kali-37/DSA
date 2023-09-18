f4a1b07d058d8734d0ad1181d952ae88d67a685b
Subash1237 <rimalsubash77@gmail.com>
2023-09-18 16:57:01 +0545

    done valid_suduko ,  and seens soln neetcode.io

--- Leetcode150/Array & Hashing/valid_sudoku.py
+++ Leetcode150/Array & Hashing/valid_sudoku.py
@@ -1,19 +1,32 @@
 # https://leetcode.com/problems/valid-sudoku/
 
+import collections
 class Solution:
     def isValidSudoku(self, board: list[list[str]]) -> bool:
-       print(board)
-        
+        rowcheck=collections.defaultdict(list)
+        columncheck=collections.defaultdict(list)
+        squarecheck=collections.defaultdict(list)
+        for r in range(9):
+            for c in range(9):
+                if board[r][c]!=".":
+                    if( board[r][c] in rowcheck[r] or board[r][c] in columncheck[c] or board[r][c] in squarecheck[(r//3,c//3)] ):
+                        print(r,c)
+                        return False
+                    rowcheck[r].add(board[r][c])
+                    columncheck[c].add(board[r][c])
+                    squarecheck[(r//3,c//3)].add(board[r][c] )
+
+        return True
 
 
 solver=Solution()
-solver.isValidSudoku(      
-[["8","3",".",".","7",".",".",".","."]   
-,["6",".",".","1","9","5",".",".","."]   
-,[".","9","8",".",".",".",".","6","."]   
-,["8",".",".",".","6",".",".",".","3"]  
-,["4",".",".","8",".","3",".",".","1"]     
-,["7",".",".",".","2",".",".",".","6"]   
-,[".","6",".",".",".",".","2","8","."]  
-,[".",".",".","4","1","9",".",".","5"]  
-,[".",".",".",".","8",".",".","7","9"]]) 
+print(solver.isValidSudoku(      
+[["5","5",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]) )
