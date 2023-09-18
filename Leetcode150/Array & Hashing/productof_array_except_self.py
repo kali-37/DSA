54bdbe2d2febfdc23f9900e926ec6d768a7ad75e
Subash1237 <rimalsubash77@gmail.com>
2023-09-18 09:23:25 +0545

    0(n) space , removing spce need for postf & prefix

--- Leetcode150/Array & Hashing/productof_array_except_self.py
+++ Leetcode150/Array & Hashing/productof_array_except_self.py
@@ -1,5 +1,29 @@
 # https://leetcode.com/problems/product-of-array-except-self/
 
+
+# SOlution 1 : 
+# class Solution:
+#     def productExceptSelf(self, nums: list[int]) -> list[int]:
+#         prefix,postfix=[],[]
+#         multiplier=1
+#         for i in nums:
+#             multiplier*=i
+#             prefix.append(multiplier)
+#         multiplier=1
+#         for i in range(len(nums)-1,0,-1):
+#             multiplier*=nums[i]
+#             postfix.insert(0,multiplier)
+#         returner=[]
+#         for i in range(1,len(nums)-1):
+#             returner.append(prefix[i-1]*postfix[i])            
+#         returner.insert(0,postfix[0])
+#         returner.append(prefix[len(nums)-2])
+#         return returner 
+
+
+# SOlution 2: is compact version of Solution without having to have extra memory space of 
+# 0(n) for both prefix and postfix array
+
 class Solution:
     def productExceptSelf(self, nums: list[int]) -> list[int]:
         res=[1]*len(nums)
