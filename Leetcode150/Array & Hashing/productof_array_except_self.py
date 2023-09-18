4baeb02988c0f6a346c667549a35bcfbbba8948c
Subash1237 <rimalsubash77@gmail.com>
2023-09-18 08:55:26 +0545

    sa..

--- Leetcode150/Array & Hashing/productof_array_except_self.py
+++ Leetcode150/Array & Hashing/productof_array_except_self.py
@@ -2,28 +2,19 @@
 
 class Solution:
     def productExceptSelf(self, nums: list[int]) -> list[int]:
-        prefix=[]
-        multiplier=1
-        postfix=[]
-        for i in nums:
-            multiplier*=i
-            prefix.append(multiplier)
-        print(prefix,"prefix")
-        multiplier=1
-        for i in range(len(nums)-1,0,-1):
-            print("i am num 0 ",nums[0])
-            multiplier*=nums[i]
+        res=[1]*len(nums)
+        print(res)
+        postfix=1   
+        prefix=1
+        for i in range(len(nums)):
+            res[i]=prefix
+            prefix*=nums[i]
 
-            postfix.insert(0,multiplier)
-
-            # postfix.insert(0,multiplier*num[0])
-        print(postfix,"postfix")
-        returner=[]
-        for i in range(1,len(nums)-1):
-            returner.append(prefix[i-1]*postfix[i])            
-        returner.insert(0,postfix[0])
-        returner.append(prefix[len(nums)-2])
-        return returner 
-
+        print(res)
+        
+        for i in range(len(nums) -1,-1,-1):
+            res[i]*=postfix
+            postfix*=nums[i]
+        return res
 solver=Solution()
-print(solver.productExceptSelf([1,2,3,4]))
+print(solver.productExceptSelf([7,2,3,4]))
