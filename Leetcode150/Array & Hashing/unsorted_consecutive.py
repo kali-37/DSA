41754a7d7bf0f1cbdf9b8e160fb4e4ff5d9d77b9
Subash1237 <rimalsubash77@gmail.com>
2023-09-19 13:12:35 +0545

    mine one ..

--- Leetcode150/Array & Hashing/unsorted_consecutive.py
+++ Leetcode150/Array & Hashing/unsorted_consecutive.py
@@ -1,24 +1,26 @@
 # link : https://leetcode.com/problems/longest-consecutive-sequence/
 
-class Solution:
-    def longestConsecutive(self, nums: list[int]) -> int:
-        res=[]
-        if(nums==[]):
-            return 0
-        count_str=[1]
-        while( len(nums)>1):
-            delemiter=1
-            s=max(nums)
-            nums.remove(s)
-            while(s-max(nums))==1:
-                delemiter+=1
-                count_str.append(delemiter)
-                if len(nums)>1:
-                    s=max(nums)
-                    nums.remove(s)
-                else:
-                    break
-        return max(count_str)
+# My solution thought it's long it causes runtime error for large numbers.
+# mine one .. 
+# class Solution:
+#     def longestConsecutive(self, nums: list[int]) -> int:
+#         nums=set(nums)
+#         if(nums is empty):
+#             return 0
+#         count_str=[1]
+#         while( len(nums)>1):
+#             delemiter=1
+#             s=max(nums)
+#             nums.remove(s)
+#             while(s-max(nums))==1:
+#                 delemiter+=1
+#                 count_str.append(delemiter)
+#                 if len(nums)>1:
+#                     s=max(nums)
+#                     nums.remove(s)
+#                 else:
+#                     break
+#         return max(count_str)
 
 
 
