29cac8a0c366b42c70e7afd112cb266661d8b4cb
Subash1237 <rimalsubash77@gmail.com>
2023-09-19 22:33:02 +0545

    Thus completed..

--- Leetcode150/Two Pointers/3_SUM.py
+++ Leetcode150/Two Pointers/3_SUM.py
@@ -4,5 +4,27 @@
 # It is extension of 2 sum , but we have another for all instances . And it can't have duplicates of all..
 
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        sort=sorted(nums)
+        print(sort)
+        # seter=set(sort)
+        res=[]
+        for index,value in  enumerate(sort):
+            if(index>0)and sort[index-1]==value:
+                    continue
+            l=index+1
+            r=len(sort)-1
+            while(l<r):
+                if (r==index):
+                    r-=1
+                if(value+sort[l]+sort[r])==0:
+                    res.append([value,sort[l],sort[r]])
+                    l+=1 
+                    r-=1
+                if(value+sort[l]+sort[r])>0:
+                    r-=1
+                if(value+sort[l]+sort[r])<0:
+                    l+=1    
+        return res
+solver=Solution()
+print(solver.threeSum([-1,0,1,2,-1,-4]))
---
+++ Leetcode150/Two Pointers/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+ # print(index,"          INDEX")
+            # if(index<len(sort)-1):
+            #     if sort[index+1]==value:
+            #         continue
