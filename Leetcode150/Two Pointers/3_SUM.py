3cda0ab0e72f652e8f200b9537ef0f68a8430919
Subash1237 <rimalsubash77@gmail.com>
2023-09-19 21:18:25 +0545

    start 3 sum py

---
+++ Leetcode150/Two Pointers/3_SUM.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/3sum/
+
+
+# It is extension of 2 sum , but we have another for all instances . And it can't have duplicates of all..
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        
--- Leetcode150/Two Pointers/two_sum-||_input_array.py
+++ Leetcode150/Two Pointers/two_sum-||_input_array.py
@@ -14,5 +14,23 @@ class Solution:
                     result.append(numbers.index(temp)+2)
                 return result
 
+
+#As it's from two pointers , let's solve it using two pointers.
+
+class Solution:
+    def twoSum(self, numbers: list[int], target: int) -> list[int]:
+        result=[]
+        l,r=0,len( numbers)-1
+
+        while l<r : 
+            if (numbers[l]+numbers[r])==target:
+                return [l+1,r+1]
+            if(numbers[l]+numbers[r])>target:
+                r=r-1
+            else:
+                l=l+1
+        
+
+
 solver=Solution()
-print(solver.twoSum([0,0,0,1,6,9],0))
+print(solver.twoSum([0,0,1,6,9],0))
