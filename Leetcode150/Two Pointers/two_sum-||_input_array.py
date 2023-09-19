34cdc82c5a6885e845630fff01cb2fb1912672e7
Subash1237 <rimalsubash77@gmail.com>
2023-09-19 16:34:52 +0545

    sdr

--- Leetcode150/Two Pointers/two_sum-||_input_array.py
+++ Leetcode150/Two Pointers/two_sum-||_input_array.py
@@ -2,5 +2,17 @@
 # https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        
+    def twoSum(self, numbers: list[int], target: int) -> list[int]:
+        result=[]
+        for index,value in enumerate(numbers):  # it takes 0(n)
+            temp=target-value
+            if temp  in numbers:   # it take 0(n) because searching in array takes time of n if it's at last.
+                result.append(index+1)
+                if  value!=numbers[index+1]:
+                    result.append(numbers.index(temp)+1)
+                else:
+                    result.append(numbers.index(temp)+2)
+                return result
+
+solver=Solution()
+print(solver.twoSum([0,0,0,1,6,9],0))
