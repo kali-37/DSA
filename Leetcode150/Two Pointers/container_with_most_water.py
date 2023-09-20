8bcb300cb2f53fe15f9b1aec12aeb9b47c71042b
Subash1237 <rimalsubash77@gmail.com>
2023-09-20 10:06:19 +0545

    SO i guessed i do it for height[pointer] part upda

--- Leetcode150/Two Pointers/container_with_most_water.py
+++ Leetcode150/Two Pointers/container_with_most_water.py
@@ -3,21 +3,20 @@
 
 class Solution:
     def maxArea(self, height: list[int]) -> int:
-        print(height,len(height))
         pointerR=len(height)-1
         res=0
         odd_even_checker=0
         while(pointerR>0):
             pointerL=0
-            while(pointerL<pointerR):
-                if(pointerL<pointerR):
-                    res=max(res,pointerL*pointerL)
+            while(height[pointerL]<height[pointerR]):
+                if(height[pointerL]<height[pointerR]):
+                    res=max(res,height[pointerL]*height[pointerL])
                 else:
-                    res=max(res,pointerR*pointerR)
+                    res=max(res,height[pointerR]*height[pointerR])
                 pointerL+=1
             pointerR-=1
         return res
 
 
 solver=Solution()
-print(solver.maxArea([1,8,6,2,5,4,8,3,7]))
+print(solver.maxArea([99,10000,1,2]))
